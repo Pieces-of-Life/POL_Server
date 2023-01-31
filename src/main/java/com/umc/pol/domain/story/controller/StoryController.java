01549ef662b3ff8595e3c20ac1cbf5135843c827
gkim <kimkm0419@naver.com>
2023-02-01 07:15:50 +0900

    POL-29 :sparkles: ADD : Add swagger related annotations in Story Controller

--- src/main/java/com/umc/pol/domain/story/controller/StoryController.java
+++ src/main/java/com/umc/pol/domain/story/controller/StoryController.java
@@ -1,6 +1,8 @@
 package com.umc.pol.domain.story.controller;
 
 import com.umc.pol.domain.story.service.StoryService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/storys")
+@SecurityRequirement(name = "Bearer")
+@Tag(name = "storys", description = "이야기 API")
 public class StoryController {
   private final StoryService storyService;
 }
