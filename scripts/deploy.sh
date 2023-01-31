ea0e8af2dab90dc743b431d23b80acb13e60fb78
gkim <kimkm0419@naver.com>
2023-02-01 03:57:00 +0900

    POL-34 🚑 FIX : Change source line in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,6 +28,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-source ~/.bash_profile
+source /etc/bash.bashrc
 
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
