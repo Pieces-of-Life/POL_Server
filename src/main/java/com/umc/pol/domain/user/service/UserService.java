7fbef496b283d24b3577a852cbb3464ac562063e
hyeonjeong Park <py0429@ewhain.net>
2023-02-01 16:06:27 +0900

    POL-14 ✨ FEAT : get user info

--- src/main/java/com/umc/pol/domain/user/service/UserService.java
+++ src/main/java/com/umc/pol/domain/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.umc.pol.domain.user.service;
 
 import com.umc.pol.domain.user.dto.UserInfoGetResponseDto;
+import com.umc.pol.domain.user.entity.User;
 import com.umc.pol.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserInfoGetResponseDto getUserInfo(long userId){
+    public UserInfoGetResponseDto getUserInfo(long userId) {
+        User userInfo = userRepository.findById(userId).orElseThrow(() ->
+                new IllegalArgumentException("유저 정보가 없습니다"));
+
+
+        UserInfoGetResponseDto userInfoGetResponseDto = UserInfoGetResponseDto.builder()
+                .nickname(userInfo.getNickname())
+                .score(userInfo.getScore())
+                .profileImgUrl(userInfo.getProfileImg())
+                .level(userInfo.getLevel())
+                .build();
+
+        return userInfoGetResponseDto;
 
     }
 
