d8a295e7a614552322915fbf6da50fa61bdd55de
hyeonjeong Park <py0429@ewhain.net>
2023-02-02 21:34:19 +0900

    POL-16 ✨ FEAT : patch background color

--- src/main/java/com/umc/pol/domain/story/controller/StoryController.java
+++ src/main/java/com/umc/pol/domain/story/controller/StoryController.java
@@ -1,18 +1,28 @@
 package com.umc.pol.domain.story.controller;
 
+import com.umc.pol.domain.story.dto.PatchBackgroundColorRequestDto;
+import com.umc.pol.domain.story.dto.PatchBackgroundColorResponseDto;
 import com.umc.pol.domain.story.service.StoryService;
+import com.umc.pol.global.response.ResponseService;
+import com.umc.pol.global.response.SingleResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/storys")
-@SecurityRequirement(name = "Bearer")
-@Tag(name = "storys", description = "이야기 API")
+@RequestMapping(value = "/story")
+
 public class StoryController {
-  private final StoryService storyService;
+    private final StoryService storyService;
+    private final ResponseService responseService;
+
+    @PatchMapping("/{storyId}/color")
+    public SingleResponse<PatchBackgroundColorResponseDto> patchBackgroundColor(@PathVariable long storyId,
+                                                                                @RequestBody PatchBackgroundColorRequestDto requestDto) {
+
+        return responseService.getSingleResponse(storyService.patchBackgroundColor(storyId, requestDto));
+    }
 }
--- src/main/java/com/umc/pol/domain/story/service/StoryService.java
+++ src/main/java/com/umc/pol/domain/story/service/StoryService.java
@@ -1,11 +1,15 @@
 package com.umc.pol.domain.story.service;
 
+import com.umc.pol.domain.story.dto.PatchBackgroundColorRequestDto;
+import com.umc.pol.domain.story.dto.PatchBackgroundColorResponseDto;
+import com.umc.pol.domain.story.entity.Story;
 import com.umc.pol.domain.story.repository.LikeRepository;
 import com.umc.pol.domain.story.repository.QnaRepository;
 import com.umc.pol.domain.story.repository.StoryRepository;
 import com.umc.pol.domain.story.repository.StoryTagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +19,17 @@ public class StoryService {
   private final StoryTagRepository storyTagRepository;
   private final QnaRepository qnaRepository;
   private final LikeRepository likeRepository;
+
+  @Transactional
+  public PatchBackgroundColorResponseDto patchBackgroundColor(long storyId,
+                                                              PatchBackgroundColorRequestDto requestDto){
+    Story story = storyRepository.findById(storyId).orElseThrow(() ->
+            new IllegalArgumentException("존재하지 않는 스토리입니다."));
+    story.updateColor(requestDto.getColor());
+
+    return PatchBackgroundColorResponseDto.builder()
+            .color(requestDto.getColor())
+            .build();
+  }
 
 }
