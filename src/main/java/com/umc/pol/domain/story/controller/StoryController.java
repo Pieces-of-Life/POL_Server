2e4c18e00c35e06b6cad0c5b64390f06e1c2014a
unknown <kjgi73k@gmail.com>
2023-02-03 16:46:08 +0900

    POL-22 ✨  FEAT : setting Swagger @Operation

--- src/main/java/com/umc/pol/domain/story/controller/StoryController.java
+++ src/main/java/com/umc/pol/domain/story/controller/StoryController.java
@@ -7,6 +7,10 @@ import com.umc.pol.domain.story.dto.PatchMainStatusResponseDto;
 import com.umc.pol.domain.story.service.StoryService;
 import com.umc.pol.global.response.ResponseService;
 import com.umc.pol.global.response.SingleResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +32,7 @@ public class StoryController {
         return responseService.getSingleResponse(storyService.patchBackgroundColor(storyId, requestDto));
     }
 
+    @Operation(summary = "대표 이야기 설정", description = "이야기의 대표 여부를 변경합니다.")
     @PatchMapping("/{storyId}/main")
     public SingleResponse<PatchMainStatusResponseDto> patchMain(@PathVariable long storyId, @RequestBody PatchMainStatusRequestDto requestDto){
 
