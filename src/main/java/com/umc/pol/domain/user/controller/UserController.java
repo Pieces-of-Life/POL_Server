722e98f34be8ab047d1806fe0cdbd231b9339293
hyeonjeong Park <py0429@ewhain.net>
2023-02-06 20:34:23 +0900

    POL-17 ✨ ADD : swigger operation

--- src/main/java/com/umc/pol/domain/user/controller/UserController.java
+++ src/main/java/com/umc/pol/domain/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.umc.pol.domain.user.repository.UserRepository;
 import com.umc.pol.domain.user.service.UserService;
 import com.umc.pol.global.response.ResponseService;
 import com.umc.pol.global.response.SingleResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class UserController {
     private final UserService userService;
 
     // 유저 정보 조회
+    @Operation(summary = "유저 정보 조회", description = "현재 유저의 정보를 조회합니다.")
     @GetMapping("/info")
     public SingleResponse<UserInfoGetResponseDto> getName() {
         // 현재 사용자의 ID return
