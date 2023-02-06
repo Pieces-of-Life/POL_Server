37b56811fe090acd8543ed06848f313b0b96ab30
hyeonjeong Park <py0429@ewhain.net>
2023-02-06 20:35:10 +0900

    POL-17 ✨ ADD : swigger operation

--- src/main/java/com/umc/pol/domain/auth/controller/KakaoAuthController.java
+++ src/main/java/com/umc/pol/domain/auth/controller/KakaoAuthController.java
@@ -2,6 +2,7 @@ package com.umc.pol.domain.auth.controller;
 
 import com.umc.pol.domain.auth.dto.KakaoInfoResponseDto;
 import com.umc.pol.domain.auth.service.KakaoAuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ public class KakaoAuthController {
     private final KakaoAuthService kakaoAuthService;
 
     // 카카오 로그인
+    @Operation(summary = "카카오 로그인", description = "카카오 로그인을 진행합니다.")
     @GetMapping("/kakao")
     public KakaoInfoResponseDto getKakaoCallback(@RequestParam("code") String code) {
         System.out.println("code = " + code);
