bf579cd1ab09decded612b3996659dea8ef16eea
gkim <kimkm0419@naver.com>
2023-02-08 18:30:42 +0900

    POL-26 ✨ FEAT : Create getStoryList in StoryService

--- src/main/java/com/umc/pol/domain/story/service/StoryService.java
+++ src/main/java/com/umc/pol/domain/story/service/StoryService.java
@@ -1,10 +1,11 @@
 package com.umc.pol.domain.story.service;
 
-import com.umc.pol.domain.story.repository.LikeRepository;
-import com.umc.pol.domain.story.repository.QnaRepository;
+import com.umc.pol.domain.story.dto.response.GetStoryResponse;
 import com.umc.pol.domain.story.repository.StoryRepository;
-import com.umc.pol.domain.story.repository.StoryTagRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +13,13 @@ import org.springframework.stereotype.Service;
 public class StoryService {
 
   private final StoryRepository storyRepository;
-  private final StoryTagRepository storyTagRepository;
-  private final QnaRepository qnaRepository;
-  private final LikeRepository likeRepository;
+
+  public List<GetStoryResponse> getStoryList(Pageable pageable, Long cursorId) {
+    List<GetStoryResponse> storyList = storyRepository.findStory(pageable, cursorId)
+      .stream().map(GetStoryResponse::new)
+      .collect(Collectors.toList());
+
+    return storyList;
+  }
 
 }
