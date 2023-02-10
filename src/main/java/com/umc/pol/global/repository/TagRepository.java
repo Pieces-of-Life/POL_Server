5316b590016bd7226ce288f644ca3998e8147fcf
gkim <kimkm0419@naver.com>
2023-02-11 04:43:18 +0900

    POL-25 :sparkles: FEAT : Create TagRepository

---
+++ src/main/java/com/umc/pol/global/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.umc.pol.global.repository;
+
+import com.umc.pol.global.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
